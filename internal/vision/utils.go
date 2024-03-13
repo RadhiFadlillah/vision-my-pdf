e9637648a32738353115ca47b8b3f8de28cb632a
Radhi Fadlillah <m.radhi.f@gmail.com>
2024-03-13 21:54:47 +0700

    Remove unneeded code

--- internal/vision/utils.go
+++ internal/vision/utils.go
@@ -2,26 +2,9 @@ package vision
 
 import (
 	"image"
-	"io"
 
 	visionpb "cloud.google.com/go/vision/v2/apiv1/visionpb"
 )
-
-func validateImage(f io.Reader) (bool, error) {
-	// Decode image
-	i, _, err := image.Decode(f)
-	if err != nil {
-		return false, err
-	}
-
-	// Make sure image not empty
-	bounds := i.Bounds().Size()
-	if valid := bounds.X > 1 && bounds.Y > 1; !valid {
-		return false, nil
-	}
-
-	return true, nil
-}
 
 func bpToRect(bp *visionpb.BoundingPoly) image.Rectangle {
 	vertices := bp.Vertices
